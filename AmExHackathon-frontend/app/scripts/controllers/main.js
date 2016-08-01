6b3b2f5204d8966ee9feae1d61305a080494f82d
Aaron Abbott <aabmass@gmail.com>
2016-08-01 17:10:24 -0400

    Deleted ember project, added yeoman angular project

---
+++ AmExHackathon-frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name amExHackathonApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the amExHackathonApp
+ */
+angular.module('amExHackathonApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
