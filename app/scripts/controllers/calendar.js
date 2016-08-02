7afdb10d3980586a9058d340b49d60f762a8128d
Justin Macedo <justinmacedo@ufl.edu>
2016-08-02 16:17:45 -0400

    added chang view calendar buttons

--- app/scripts/controllers/calendar.js
+++ app/scripts/controllers/calendar.js
@@ -10,9 +10,12 @@
 angular.module('amExHackathonApp')
   .controller('CalendarCtrl', function ($scope) {
   	$scope.calendarView = 'month';
-  	var date = new Date();
+  	var calDate = new Date();
   	$scope.calendarDate = calDate;
   	$scope.calendarTitle = calDate.getMonth() + " " + calDate.getFullYear();
+  	$scope.changeView = function(newView){
+  		$scope.calendarView = newView;
+  	};
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
