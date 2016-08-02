33c2299ed2b78d20bf06634b6919d11a5550f2a7
Aaron Abbott <aabmass@gmail.com>
2016-08-02 10:51:04 -0400

    Cleaned up server from ember stuf

--- api/server.js
+++ api/server.js
@@ -6,23 +6,13 @@ var models = require("./models");
 
 var routes = require('./routes/index');
 
-//var controllers = require('./controllers');
-//var middleware = require('./middleware');
-
 var app = express();
 
 // mount middlewares
 app.use(expressLogging(logger));
-//app.use(middleware.emberHeadersMiddleware);
 
 // mount the controllers router
-//app.use('/api', controllers);
 app.use('/', routes);
-
-// let port = 4500;
-// app.listen(port, () => {
-//   console.log("Express server running on port " + port);
-// });
 
 //sync the models with the data, will creat the schems if not present
 //starts the server
