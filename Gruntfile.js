10dd832f30bb8bac4e5c6dec74775e37666e2580
Aaron Abbott <aabmass@gmail.com>
2016-08-03 00:15:54 -0400

    Open browser automatically

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,7 +34,30 @@ module.exports = function (grunt) {
     // Run the express backend
     nodemon: {
       dev: {
-        script: 'api/server.js'
+        script: 'api/server.js',
+        options: {
+          callback: function(nodemon) {
+            nodemon.on('log', function (event) {
+              console.log(event.colour);
+            });
+
+            // opens browser on initial server start 
+            nodemon.on('config:update', function () {
+              // Delay before server listens on port 
+              setTimeout(function() {
+                require('open')('http://localhost:4500');
+              }, 250);
+            });
+
+            // refreshes browser when server reboots 
+            nodemon.on('restart', function () {
+              // Delay before server listens on port 
+              setTimeout(function() {
+                require('fs').writeFileSync('.rebooted', 'rebooted');
+              }, 1000);
+            });
+          }
+        }
       }
     },
 
