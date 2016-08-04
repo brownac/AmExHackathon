36c6cb51f22793a7fd495bd3df628e64781c0601
Aaron Abbott <aabmass@gmail.com>
2016-08-04 16:47:56 -0400

    File upload working in angular

--- app/scripts/controllers/candidateForm.js
+++ app/scripts/controllers/candidateForm.js
@@ -10,9 +10,25 @@
 angular.module('amExHackathonApp')
   .controller('CandidateFormCtrl', function ($scope, $q, $timeout, candidateService) {
     const init = function() {
+      $scope.postCandidate = {};
       $scope.pictureAdded = false;
       $scope.buttonText = "Submit";
       $scope.submitBtnClasses = "btn btn-primary";
+    };
+
+    $scope.readImage = function(event) {
+      var files = event.target.files;
+
+      if (files && files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+          $scope.$apply(function() {
+            $scope.postCandidate.resumeBase64 = e.target.result;
+            $scope.pictureAdded = true;
+          });
+        };
+        reader.readAsDataURL(files[0]);
+      }
     };
 
     $scope.submit = function() {
