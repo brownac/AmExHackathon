ac2ac842438679520ef822015194a0019e6e9f7a
alechavez93 <alechavez93@gmail.com>
2016-08-04 22:54:21 -0400

    Highlighter working and tools record state, added other components

--- softtouch/controller.js
+++ softtouch/controller.js
@@ -1,12 +1,14 @@
 //Module Fields
 //-------------------------------------------------------------------------------------
-var colors = {red: "#ff0000", blue: "#0000cc", yellow: "#ffff66", black: "#000000", green: "#33ff78"};
+var colors = {red: "rgba(255,0,0,1)", blue: "rgba(0,0,204,1)", yellow: "rgba(255,255,102,1)", black: "rgba(0,0,0,1)", green: "rgba(51,255,120,1)"};
 	defaultPenColor = "black";
 	defaultMarkerColor = "yellow";
 	penThickOptions = [1, 5, 10];
 	markerThickOptions = [20, 35, 50];
+	markerOppacity = 0.2;
 	penSelectedThickness = 0;
 	markerSelectedThickness = 0;
+
 
 var colorButtons = $(".color-button");
 	toolButtons = $(".tool-button");
@@ -57,20 +59,46 @@ function redo() {
 }
 
 function changeColor(color){
-	canvas.freeDrawingBrush.color = colors[color];
+	if(($("#pen")[0].getAttribute("class")).indexOf("active") !== -1){
+		canvas.freeDrawingBrush.color = colors[color];
+		defaultPenColor = color;
+		
+	} else{
+		var newColor = colors[color].split(",");
+		newColor[3] = markerOppacity + ")";
+		console.log(newColor.join());
+		canvas.freeDrawingBrush.color = newColor.join();
+		defaultMarkerColor = color;
+	}
+}
+
+function activateTag(tag){
+	tag.className += " active";
+}
+
+function updateThicknessDisplay(thickness){
+	$("#thickness").text(thickness + 1);
+}
+
+function findTagByColor(color){
+	for(var i=0; i<colorButtons.length; i++){
+		if(colorButtons[i].value == color)
+			return colorButtons[i];
+	}
+	return {class: "", value: ""};
 }
 
 function increaseWidth(){
 	if($("#pen").hasClass("active")){
 		if(penSelectedThickness < penThickOptions.length - 1){
 			canvas.freeDrawingBrush.width = penThickOptions[++penSelectedThickness];
-			$("#thickness").text(penSelectedThickness + 1);
+			updateThicknessDisplay(penSelectedThickness);
 		}
 	}
 	else if($("#marker").hasClass("active")){
-		if(penSelectedThickness < markerThickOptions.length - 1){
+		if(markerSelectedThickness < markerThickOptions.length - 1){
 			canvas.freeDrawingBrush.width = markerThickOptions[++markerSelectedThickness];
-			$("#thickness").text(markerSelectedThickness + 1);
+			updateThicknessDisplay(markerSelectedThickness);
 		}
 	}
 }
@@ -79,49 +107,60 @@ function decreaseWidth(){
 	if($("#pen").hasClass("active")){
 		if(penSelectedThickness > 0){
 			canvas.freeDrawingBrush.width = penThickOptions[--penSelectedThickness];
-			$("#thickness").text(penSelectedThickness + 1);
+			updateThicknessDisplay(penSelectedThickness);
 		}
 	}
 	else if($("#marker").hasClass("active")){
-		if(penSelectedThickness > 0){
-			canvas.freeDrawingBrush.width = penThickOptions[--penSelectedThickness];
-			$("#thickness").text(penSelectedThickness + 1);
+		if(markerSelectedThickness > 0){
+			canvas.freeDrawingBrush.width = markerThickOptions[--markerSelectedThickness];
+			updateThicknessDisplay(markerSelectedThickness);
 		}
 	}
 }
 
+
+
 //Event Listeners
 // --------------------------------------------------------------------------------
 $("#undo").on("click", function(){
-	console.log("undo");
 	undo();
 });
 
 $("#redo").on("click", function(){
-	console.log("redo");
 	redo();
 });
 
 $(".color-button").on("click", function(){
-	console.log("changing color to " + this.value);	
-	$(".color-button").removeClass("active");
-	this.className += " active";
+	$(".color-button").removeClass("active");	
 	changeColor(this.value);
+	activateTag(this);
 });
 
 $(".thinknessChanger").on("click", function(){
-	console.log("changing thickness");
 	if(this.value == "plus"){
 		console.log("increase");
 		increaseWidth();
-	}else {
+	} else {
 		console.log("decrease");
 		decreaseWidth();
 	}
 });
 
 $(".tool-button").on("click", function(){
-	console.log("changing tool");
 	$(".tool-button").removeClass("active");
 	this.className += " active";
+
+	if(this.id == 'pen'){
+		$(".color-button").removeClass("active");
+		canvas.freeDrawingBrush.width = penThickOptions[penSelectedThickness];
+		updateThicknessDisplay(penSelectedThickness);
+		changeColor(defaultPenColor);
+		activateTag(findTagByColor(defaultPenColor));
+	} else{
+		$(".color-button").removeClass("active");
+		canvas.freeDrawingBrush.width = markerThickOptions[markerSelectedThickness];		
+		updateThicknessDisplay(markerSelectedThickness);
+		changeColor(defaultMarkerColor);
+		activateTag(findTagByColor(defaultMarkerColor));
+	}
 });
