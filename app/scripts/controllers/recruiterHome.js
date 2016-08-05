eaa1126b326e11e30f0d5fa3fa78dd1e1e5721d7
justintime5046 <justintime5046@gmail.com>
2016-08-05 16:24:19 -0400

    name split and queue/calendar communication

--- app/scripts/controllers/recruiterHome.js
+++ app/scripts/controllers/recruiterHome.js
@@ -8,19 +8,15 @@
  * Controller of the amExHackathonApp
  */
 angular.module('amExHackathonApp')
-  .controller('RecruiterHomeCtrl', function ($scope) {
-  	$scope.candidateQueue = [
-	    {firstName:'John', lastName:'Doe', age:25, gender:'boy'},
-	    {firstName:'Jessie', lastName:'Adams', age:30, gender:'girl'},
-	    {firstName:'Johanna', lastName:'Davids', age:28, gender:'girl'},
-	    {firstName:'Joy', lastName:'Express', age:15, gender:'girl'},
-	    {firstName:'Mary', lastName:'Airlines', age:28, gender:'girl'},
-	    {firstName:'Peter', lastName:'Royal', age:95, gender:'boy'},
-	    {firstName:'Sebastian', lastName:'Cruise', age:50, gender:'boy'},
-	    {firstName:'Erika', lastName:'Fish', age:27, gender:'girl'},
-	    {firstName:'Patrick', lastName:'Cat', age:40, gender:'boy'},
-	    {firstName:'Samantha', lastName:'Dog', age:60, gender:'girl'}
-  	];
+  .controller('RecruiterHomeCtrl', function ($scope, candidateService) {
+
+    /*Query DB for candidates with finalDecision == scheduleInterview*/
+    candidateService.query().$promise.then(values => {
+      $scope.candidateQueue = values;
+      console.log(values);
+    });
+    /*End Query*/
+
   	$scope.interviewTime = '9:00 AM';
   	$scope.scheduledCandidates = [];
   	$scope.calendarView = 'month';
