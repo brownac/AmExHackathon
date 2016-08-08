133f2fc083bd7f5f79321d2f1cd7a046a929e3ca
Aaron Abbott <aabmass@gmail.com>
2016-08-08 16:27:34 -0400

    Get image on next button, softpen service

--- app/scripts/controllers/softpen.js
+++ app/scripts/controllers/softpen.js
@@ -1,9 +1,11 @@
 'use strict';
 
-
 var app = angular.module('amExHackathonApp');
-app.controller('SoftPenCtrl', ['$scope', function($scope) {
+app.controller('SoftPenCtrl', function($scope, softpenImage) {
   $scope.src = '../../images/resume.png';
 
-
-}]);
+  $scope.next = function() {
+    var imgBase64 = $("#edited-image").attr('src');
+    softpenImage.src = imgBase64;
+  }
+});
