5991631b5d1ee349285d70ad857b2591492d093f
Aaron Abbott <aabmass@gmail.com>
2016-08-08 17:04:19 -0400

    Working again, cleaned up code from jquery

--- app/scripts/controllers/softpen.js
+++ app/scripts/controllers/softpen.js
@@ -5,10 +5,14 @@ app.controller('SoftPenCtrl', function($scope, $location, softpenImage) {
   $scope.src = '../../images/resume.png';
 
   $scope.next = function() {
-    var imgBase64 = $("#edited-image").attr('src');
-    softpenImage.src = imgBase64;
+    var canvas = $("#c")[0];
+    var image = new Image();
+    image.src = canvas.toDataURL("image/png");
 
-    // redirect to form
+    // set the softpenImage.src to the base64 image from canvas
+    softpenImage.src = image.src;
+
+    // redirect to form, which uses softpenImage
     $location.path('screener/candidateForm');
   }
 });
