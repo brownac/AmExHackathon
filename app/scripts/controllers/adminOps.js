f0b1029cd98e361b82e0f828c0e2e56c17075fd1
Kinderley Charles <kinderleycharles@aol.com>
2016-08-09 15:13:42 -0400

    Front end of Administration Panel to add form is done

--- app/scripts/controllers/adminOps.js
+++ app/scripts/controllers/adminOps.js
@@ -10,5 +10,28 @@
 
 
 angular.module('amExHackathonApp').controller('adminOptionsCtrl', function($scope) {
-
+  $scope.forms = [{
+    active: true,
+    type: 'Technical',
+    form: 'A',
+    version: 1.0
+  },
+  {
+    active: false,
+    type: 'Technical',
+    form: 'B',
+    version: 1.0
+  },
+  {
+    active: false,
+    type: 'Behavioral',
+    form: 'A',
+    version: 2.0
+  },
+  {
+    active: true,
+    type: 'Behavioral',
+    form: 'B',
+    version: 2.0
+  }];
 });
