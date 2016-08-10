58d09ad4453497e19ec8f0661954e786a0c7296e
Kinderley Charles <kinderleycharles@aol.com>
2016-08-09 23:27:38 -0400

    Front end for adding new interviews questions forms done. input validation added as well as angular integration but everything is static

--- app/scripts/controllers/adminOps.js
+++ app/scripts/controllers/adminOps.js
@@ -34,4 +34,33 @@ angular.module('amExHackathonApp').controller('adminOptionsCtrl', function($scop
     form: 'B',
     version: 2.0
   }];
+
+  $scope.toggleActive = function(form) {
+    var i = $scope.forms.indexOf(form);
+    $scope.forms[i].active = !form.active;
+
+    // Call server size function to make server-side update
+  };
+
+  $scope.initNewForm = function(){
+    $scope.newForm = {
+      active: false,
+      type: '',
+      form: '',
+      version: 0,
+      files: []
+    };
+  };
+
+  $scope.submit = function(form) {
+    // Flag denoted that the form has been subitted
+    $scope.submitted = true;
+
+    // If one of the input validation fails, reject submission
+    if (form.$invalid) {
+      return;
+    }
+
+  };
+
 });
