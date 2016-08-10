5aeceb5a53ec8b1ba44914ed4dfeea0e03dfc68c
Francis Perez <fcsp19191993@ufl.edu>
2016-08-09 21:20:17 -0400

    Resolving merge conflict

--- app/scripts/controllers/interviews.js
+++ app/scripts/controllers/interviews.js
@@ -8,21 +8,38 @@
  * Controller of the amExHackathonApp
  */
 angular.module('amExHackathonApp')
-  .controller('InterviewsCtrl', function ($scope) {
+  .controller('InterviewsCtrl', function ($scope, calendarService) {
     $scope.events = [];
     $scope.calendarView = 'month';
     var calDate = new Date();
     $scope.calendarDate = calDate;
     $scope.calendarTitle = calDate.getMonth() + " " + calDate.getFullYear();
 
-    $scope.events.push({
-        title:"JUSTIN MACEDO",
-        startsAt: new Date(),
-        color: {
-          primary: '#e3bc08',
-          secondary: '#fdf1ba'
+    var query = {
+      sequelize:{
+        interview_Date:{
+          $not: null
         }
-      });
+      }
+    };
+
+    calendarService.query(query).$promise.then(values => {
+      $scope.scheduledCandidates = values;
+      for (var i = $scope.scheduledCandidates.length - 1; i >= 0; i--) {
+        $scope.events.push({
+          title:    $scope.scheduledCandidates[i].lastName + ', ' + 
+                    $scope.scheduledCandidates[i].firstName + ': ' +       
+                    $scope.scheduledCandidates[i].Interview.interview_Location,
+          startsAt: $scope.scheduledCandidates[i].Interview.interview_Date,
+          color: {
+             primary: '#e3bc08',
+             secondary: '#fdf1ba'
+           },
+           candidate: $scope.scheduledCandidates[i]
+         });
+     };
+     });
+
     $scope.changeView = function(newView){
       $scope.calendarView = newView;
     };
