be74ad5f51ee8a7b686f0f76baec863ab687ec0e
tyang82 <tyang82@gatech.edu>
2016-08-11 11:37:40 -0400

    modify calendar

--- app/scripts/controllers/interviews.js
+++ app/scripts/controllers/interviews.js
@@ -10,7 +10,7 @@
 angular.module('amExHackathonApp')
   .controller('InterviewsCtrl', function ($scope,$location, calendarService) {
     $scope.events = [];
-    $scope.calendarView = 'month';
+    $scope.calendarView = 'day';
     var calDate = new Date();
     $scope.calendarDate = calDate;
     $scope.calendarTitle = calDate.getMonth() + " " + calDate.getFullYear();
@@ -27,11 +27,16 @@ angular.module('amExHackathonApp')
     calendarService.query(query).$promise.then(values => {
       $scope.scheduledCandidates = values;
       for (var i = $scope.scheduledCandidates.length - 1; i >= 0; i--) {
+        var endDate = new Date($scope.scheduledCandidates[i].Interview.interview_Date);
+        endDate.setHours(endDate.getHours() + 1, endDate.getMinutes());
+        console.log(endDate);
+        console.log($scope.scheduledCandidates[i].Interview.interview_Date);
         $scope.events.push({
           title:    $scope.scheduledCandidates[i].lastName + ', ' +
                     $scope.scheduledCandidates[i].firstName + ': ' +
                     $scope.scheduledCandidates[i].Interview.interview_Location,
-          startsAt: $scope.scheduledCandidates[i].Interview.interview_Date,
+          startsAt: new Date($scope.scheduledCandidates[i].Interview.interview_Date),
+          endsAt: endDate,
           color: {
              primary: '#e3bc08',
              secondary: '#fdf1ba'
